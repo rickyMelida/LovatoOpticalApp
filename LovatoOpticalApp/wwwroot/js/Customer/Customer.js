dc50701cd26f961aa49c23ce5d3ee635ddbbae63
Ricardo Melida <40615609+rickyMelida@users.noreply.github.com>
2026-07-26 22:23:57 -0300

    has been added customer JS file

---
+++ LovatoOpticalApp/wwwroot/js/Customer/Customer.js
@@ -0,0 +1,8 @@
+﻿import { initForm } from './Customer.Form.js'
+import { enableButton } from '../Common/ButtonEvents.js'
+import { handleGridModal } from './Customer.Grid.js'
+import { handlerCustomer } from './Customer.Recipe.js'
+
+initForm();
+handleGridModal();
+handlerCustomer();
